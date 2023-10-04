ebcc50c31d18ac1163566c4a54407c6d08555201
Domiziano De Santis <119798750+DeSantisDomiziano@users.noreply.github.com>
2023-10-04 14:48:52 +0200

    first commit

---
+++ src/main/java/org/java/app/pojo/Pizza.java
@@ -0,0 +1,78 @@
+package org.java.app.pojo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Pizza {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(length = 60, nullable = false)
+	private String name;
+	
+	@Column(length = 150)
+	private String overview;
+	private String picture;
+	
+	@Column(nullable = false)
+	private float price;
+
+	public Pizza(int id, String name, String overview, String picture, float price) {
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getOverview() {
+		return overview;
+	}
+
+	public void setOverview(String overview) {
+		this.overview = overview;
+	}
+
+	public String getPicture() {
+		return picture;
+	}
+
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Pizza [id=" + id + ", name=" + name + ", overview=" + overview + ", picture=" + picture + ", price="
+				+ price + "]";
+	}
+	
+	
+
+}
