60de7e2e572fc226dc8efff7d96718fbd6e77a8e
Domiziano De Santis <119798750+DeSantisDomiziano@users.noreply.github.com>
2023-10-06 12:28:47 +0200

    add route create & file with field

--- src/main/java/org/java/app/controller/MainController.java
+++ src/main/java/org/java/app/controller/MainController.java
@@ -7,10 +7,15 @@ import org.java.app.serve.PizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/")
@@ -40,5 +45,28 @@ public class MainController {
             model.addAttribute("pizza", pizza);
             return "show";
     }
+	
+	@GetMapping("/create")
+	public String getCreate(Model model) {
+		
+		model.addAttribute("pizza", new Pizza());
+		
+		return "create";
+	}
+	
+	@PostMapping("/create")
+	public String storePizza(@Valid @ModelAttribute Pizza pizza,
+							BindingResult bindingResult) {
+		
+		if(bindingResult.hasErrors()) {
+			bindingResult.getAllErrors().forEach(System.out::println);
+			
+			return "create";
+		}
+		
+		pizzaService.save(pizza);
+		
+		return "redirect:/pizze";
+	}
 
 }
